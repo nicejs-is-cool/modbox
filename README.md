75b79d1b937ebcf52a1fbbe19e2c18303aeede3d
nicejs-is-cool <xbgxtmp+nicejsverif@gmail.com>
2022-03-29 12:57:54 -0300

    add install instructions and installer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rmtb-modbox
+A mod system for rmtrollbox
+### Install
+The installation is simple, just run this command on rmtrollbox:
+```
+/eval $loader.script("https://github.com/nicejs-is-cool/modbox/raw/main/install.js");
+```
---
+++ install.js
@@ -0,0 +1,10 @@
+localStorage.uncatwrongboot = "$loader.script('https://github.com/nicejs-is-cool/modbox/raw/main/dist/bundle.js')";
+window.printMsg({
+    nick: 'modbox',
+    color: 'blue',
+    home: '',
+    style: '',
+    msg: 'Installation successful! Reloading now...',
+    date: Date.now()
+})
+location.reload();
