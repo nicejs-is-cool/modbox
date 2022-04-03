d462de79cd85585093596878540665ca8bbf2a08
nicejs-is-cool <xbgxtmp+nicejsverif@gmail.com>
2022-04-03 11:00:38 -0300

    i forgor to git add

---
+++ examples/alert.js
@@ -0,0 +1,9 @@
+class AlertMod extends modbox.Mod {
+    name = "AlertMod";
+    description = "me when the alert"
+    version = "1.0.0"
+    namespace = "alert-mod";
+    init() {
+        alert('helo');
+    }
+}
